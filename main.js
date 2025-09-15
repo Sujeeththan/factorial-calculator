56319b943bfcd6a140c8ae66c7792ba1bc6452d4
sujeeththan <sujeethsujeeth487@gmail.com>
2025-09-15 23:00:04 +0530

    modified

---
+++ main.js
@@ -0,0 +1,20 @@
+function Calc(event) {
+  event.preventDefault();
+//   alert('hello')
+
+const num1 = document.getElementById('num1').value;
+const output = document.getElementById('output');
+const  f = 1;
+
+if (num1 === '' || num1 < 0) {
+    output.value = 'Please enter a valid number!';
+    return;
+} else{
+    for( i = 1; i <= num1; i++)  f *= i;{
+     document.getElementById('output').innerHTML = f;
+     output.value = `${num1}! = ${factorial}`;
+}
+}
+
+
+}
